a785108a4bafeb76eb6b347dcdae1eb07abd5591
levitaes <eric.d.bos@icloud.com>
2025-03-11 12:36:22 +0100

    chore: update terminal url

--- code/winbox.apps.js
+++ code/winbox.apps.js
@@ -8,7 +8,7 @@ function terminalApp () {
         height: "45%",
         minwidth: "400px",
         minheight: "300px",
-        url: "https://thief.lerchen.net/thief-os/",
+        url: "https://levitaes.net/terminal/",
     });
 }
 
